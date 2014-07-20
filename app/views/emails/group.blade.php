5a5446b8d4a999bb8967c5e8e171ca5be2f34465
Sayak Banerjee <sayakb@kde.org>
2014-07-20 03:32:22 -0500

    Correct array index for title

--- app/views/emails/group.blade.php
+++ app/views/emails/group.blade.php
@@ -6,7 +6,7 @@
 	<p>{{{ $justification }}}</p>
 @endif
 
-<p>{{ Lang::get('email.link_login', array('title', Config::get('app.title'))) }}</p>
+<p>{{ Lang::get('email.link_login', array('title' => Config::get('app.title'))) }}</p>
 <p>{{ url('auth/login') }}</p>
 
 <hr />
